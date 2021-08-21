33fa22505c89e40d3adbbc8b385c9945e804a11b
Revivedaniel <mlg.danielstark@gmail.com>
2021-08-20 20:55:32 -0700

    Created second test for getName for invalid input

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,8 +1,15 @@
 const Employee = require("../lib/Employee")
 
 describe("Employee", () => {
+    //Testing that the getName function returns the correct name when given a name in the form of a string
     it("Should return Daniel", () => {
         const obj = new Employee("Daniel", 1, "Daniel@fakeemail.com");
         expect(obj.getName()).toBe("Daniel")
+    })
+    //Testing that the getName function returns an error if the name passed to Employee isnt a string
+    it("Should return an error", () => {
+        const obj = new Employee(1, 1, "Daniel@fakeemail.com");
+        const err = new Error("Name needs to be a string");
+        expect(() => obj.getName()).toThrow(err)
     })
 })
