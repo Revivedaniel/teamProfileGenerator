2b688bc0401681172e7290f5ceff8d225afd9912
Revivedaniel <mlg.danielstark@gmail.com>
2021-08-20 20:59:31 -0700

    Created first test for getId with valid input

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -7,9 +7,14 @@ describe("Employee", () => {
         expect(obj.getName()).toBe("Daniel")
     })
     //Testing that the getName function returns an error if the name passed to Employee isnt a string
-    it("Should return an error", () => {
+    it("Should return an error of Name needs to be a string", () => {
         const obj = new Employee(1, 1, "Daniel@fakeemail.com");
         const err = new Error("Name needs to be a string");
         expect(() => obj.getName()).toThrow(err)
+    })
+    //Testing that the getId function returns correct id when given a number
+    it("Should return Daniel@fakeemail.com", () => {
+        const obj = new Employee("Daniel", 1, "Daniel@fakeemail.com");
+        expect(obj.getId()).toBe(1)
     })
 })
