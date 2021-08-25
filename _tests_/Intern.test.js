45bec5a9e58f2cfcebdc7938987dc8a680919298
Revivedaniel <mlg.danielstark@gmail.com>
2021-08-24 20:17:48 -0700

    Created 2nd test for Intern that getRole returns Intern

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -6,4 +6,9 @@ describe("Intern", () => {
         const obj = new Intern("Daniel", 1, "Daniel@fakeemail.com", "UCI")
         expect(obj.getSchool()).toBe("UCI")
     })
+    //Testing that the getRole method returns Intern
+    it("Should return Intern", () => {
+        const obj = new Intern("Daniel", 1, "Daniel@fakeemail.com", "UCI")
+        expect(obj.getRole()).toBe("Intern")
+    })
 })
