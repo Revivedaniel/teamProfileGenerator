c999ccbc8417560e7db90fd22808445fcdc7e50f
Revivedaniel <mlg.danielstark@gmail.com>
2021-08-24 19:59:08 -0700

    Created 2nd test for Engineer that getRole should be Engineer

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -6,4 +6,9 @@ describe("Engineer", () => {
         const obj = new Engineer("Daniel", 1, "Daniel@fakeemail.com", "Revivedaniel")
         expect(obj.getGitHub()).toBe("github.com/Revivedaniel")
     })
+    //Testing that getRole returns Engineer
+    it("Should return Engineer", () => {
+        const obj = new Engineer("Daniel", 1, "Daniel@fakeemail.com", "Revivedaniel")
+        expect(obj.getRole()).toBe("Engineer")
+    })
 })
